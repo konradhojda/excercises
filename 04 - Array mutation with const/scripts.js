8a8c55965526d1a08fbb84c52555a7951972f549
Konrad <konrad.hojda@gmail.com>
2018-10-29 17:07:09 +0100

    default change

--- 04 - Array mutation with const/scripts.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const arrayWithRandomNumbers = [5,6,7];
-// arrayWithRandomNumbers = [2,5,3]; - error because of reassign
-// but i can do something like this:
-
-arrayWithRandomNumbers[0] = 2;
-
-console.log(arrayWithRandomNumbers);
