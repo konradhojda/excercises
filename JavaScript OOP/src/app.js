884cc1bc3118e991f33dc2de082a4d2b86ecd630
Konrad <konrad.hojda@gmail.com>
2018-11-20 11:05:07 +0100

    showing results from json file

--- JavaScript OOP/src/app.js
+++ JavaScript OOP/src/app.js
@@ -6,10 +6,11 @@ import {FleetDataService} from "./services/fleet-data-service.js";
 import {Button} from "./ui/button.js";
 import {Image} from "./ui/image.js";
 import {TitleBar} from "./ui/title-bar.js";
+import {DataTable} from "./ui/data-table.js";
 
-let tb = new TitleBar('App');
-tb.addLink('Home', '');
-tb.addLink('Cars', '');
-tb.addLink('Drones', '');
-tb.addLink('Map', '');
-tb.appendToElement(jQuery('body'));
+
+let headers = 'License Make Model Miles'.split(' ');
+let dataService = new FleetDataService();
+dataService.loadData(fleet);
+let dt = new DataTable(headers, dataService.cars);
+dt.appendToElement(jQuery('body'));
